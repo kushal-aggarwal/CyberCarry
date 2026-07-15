0e9ac7afec2a8f008d51179056aa6d675bec61a4
Kushal Aggarwal <kushalaggarwalofficial@gmail.com>
2026-07-15 23:12:56 +0530

    Configure MongoDB Atlas and migrate database

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 const config = require("config");
 const dbgr = require("debug")("development:mongoose");
 
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 mongoose
-.connect(`${config.get("MONGODB_URI")}/CyberBag`)
+.connect(config.get("MONGODB_URI"))
 .then(function() {
     dbgr("connected");
 })
