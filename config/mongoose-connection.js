c9588050ceb1ff2a0d713665130bdcc09539da3c
Kushal Aggarwal <kushalaggarwalofficial@gmail.com>
2026-07-15 23:26:03 +0530

    Use environment variables for MongoDB connection

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -5,13 +5,15 @@ const dbgr = require("debug")("development:mongoose");
 const dns = require("node:dns");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
+const mongoURI = process.env.MONGODB_URI || config.get("MONGODB_URI");
+
 mongoose
-.connect(config.get("MONGODB_URI"))
-.then(function() {
+  .connect(mongoURI)
+  .then(() => {
     dbgr("connected");
-})
-.catch(function(err) {
+  })
+  .catch((err) => {
     dbgr(err);
-})
+  });
 
 module.exports = mongoose.connection;
