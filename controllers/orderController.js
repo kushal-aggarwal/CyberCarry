3e753a2414fe0a33d6d01cdb4bebfa0c07b772ff
Kushal Aggarwal <kushalaggarwalofficial@gmail.com>
2026-07-16 12:54:49 +0530

    BREVO BRAVO PT2

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const userModel = require("../models/user-model");
 const orderModel = require("../models/order-model");
-const transporter = require("../config/nodemailer");
+const transporter = require("../config/transporter");
 const { generateToken } = require("../utils/generateToken");
 
 module.exports.checkout = async function(req, res) {
