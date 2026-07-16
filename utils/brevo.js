d66e8e5acb8ac1625b1fe12ee696ae3089ecfdd5
Kushal Aggarwal <kushalaggarwalofficial@gmail.com>
2026-07-16 15:35:08 +0530

    BREVO BRAVO PT7

--- utils/brevo.js
+++ utils/brevo.js
@@ -1,10 +1,5 @@
 const brevo = require("@getbrevo/brevo");
 
-const apiInstance = new brevo.TransactionalEmailsApi();
+console.log(brevo);
 
-apiInstance.setApiKey(
-    brevo.TransactionalEmailsApiApiKeys.apiKey,
-    process.env.BREVO_SMTP_KEY
-);
-
-module.exports = apiInstance;
+module.exports = {};
